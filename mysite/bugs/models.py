c1689608d8358557f554bcd0c55b77811f33f5e8
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-10-08 18:28:11 -0400

    database setup and database with new revise from django branch

--- mysite/bugs/models.py
+++ mysite/bugs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Bugs(models.Model):
+    description = models.CharField(max_length=200)
+    bug_type = models.CharField(max_length=200)
+    report_date = models.DateTimeField("date published")
+    status = models.CharField(max_length=200)
