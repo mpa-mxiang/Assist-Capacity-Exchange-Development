e51e85217dcfe83cda2f1399d34ef1be6142a44b
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-10-10 10:10:17 -0400

    delete app name from urls

--- mysite/bugs/urls.py
+++ mysite/bugs/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
 from . import views
-app_name = 'bugs'
-
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:bug_id>/", views.detail, name="detail"),
