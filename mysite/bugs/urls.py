2a9d285326613dd5c0fa98a5602abaa9dd380196
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-10-09 23:31:24 -0400

    add action attribute for register form

--- mysite/bugs/urls.py
+++ mysite/bugs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+app_name = 'bugs'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:bug_id>/", views.detail, name="detail"),
--- mysite/bugs/views.py
+++ mysite/bugs/views.py
@@ -1,17 +1,19 @@
-from django.http import HttpResponse, HttpResponseRedirect
 from .models import Bugs
 from django.shortcuts import get_object_or_404, render
 
 def index(request):
+    print(request)
     latest_bug_list = Bugs.objects.order_by("-report_date")[:5]
     context = {"latest_bug_list": latest_bug_list}
     return render(request, "bugs/index.html", context)
 
 def detail(request, bug_id):
+    print(request)
     bug = get_object_or_404(Bugs, pk=bug_id)
     return render(request, "bugs/detail.html", {"bug": bug})
 
 def register(request):
+    print(request)
     if request.method == 'POST':
         description = request.POST['description']
         bug_type = request.POST['bug_type']
@@ -20,6 +22,6 @@ def register(request):
         bug = Bugs(description=description, bug_type=bug_type, status=status)
         bug.save()
 
-        return HttpResponseRedirect(f'/bugs/{bug.id}/')
+        return render(request, "bugs/detail.html", {"bug": bug})
 
     return render(request, "bugs/index.html")
