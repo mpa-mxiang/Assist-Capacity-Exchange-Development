0bd20cf871443e69a5c8e27fb601cba6bf6799e4
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-10-10 22:52:13 -0400

    add one more test for model

--- mysite/bugs/tests.py
+++ mysite/bugs/tests.py
@@ -33,3 +33,9 @@ class BugsModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_bug = Bugs(report_date=time)
         self.assertIs(recent_bug.was_published_recently(), True)
+    def test_bug_str_method(self):
+        """
+        __str__() method should return a string representation of the bug's type.
+        """
+        bug = Bugs(bug_type="Test Bug")
+        self.assertEqual(str(bug), "Test Bug")
