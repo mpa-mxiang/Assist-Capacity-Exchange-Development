0ac2c76a09f4e4a6a2607a467c140c5da48f7580
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-10-25 12:36:27 -0400

    change view based on model type change

--- mysite/bugs/models.py
+++ mysite/bugs/models.py
@@ -5,9 +5,7 @@ from django.utils import timezone
 
 class Bugs(models.Model):
     description = models.CharField(max_length=200)
-    bug_type = models.CharField(max_length=200)
     report_date = models.DateTimeField("date published", default=timezone.now)
-    status = models.CharField(max_length=200)
     bug_type = [
         ('error', "Error"),
         ('new_feature', "New Feature"),
